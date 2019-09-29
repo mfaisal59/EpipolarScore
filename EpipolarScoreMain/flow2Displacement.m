4c32f4a528487477f166e457d8ecdb8a2a1c8e67
Muhammad Faisal <muhammadfaisalcs@gmail.com>
2019-09-29 22:47:10 +0500

    Update flow2Displacement.m

--- EpipolarScoreMain/flow2Displacement.m
+++ EpipolarScoreMain/flow2Displacement.m
@@ -1,29 +1,15 @@
-% set up:
-% add path for trifocal folder and compile snic_mex.cpp
-% addpath('../OF+MS/mycodes/external/trifocal');
-%
-% create a root dirctory containing forward and backward flow and 
-% the images. These folders further contain subfolders for each 
-% of the sequences. Also create the folder, 'fgHeatMap'. This would
-% contain the heat maps for your seqeunces.
 clear;
 clc;
-imRepo.root = '/home/cvlab/FAISAL/DAVIS_Dataset/DAVIS-trainval-480p/';
-% imRepo.root = '/media/itu/E6540E96540E699F/SegTrackv2/';
+imRepo.root = './/DAVIS_Dataset/DAVIS-trainval-480p/';
 imRepo.floPathBwd = [imRepo.root, 'fullFlow/davisbackward/'];
 imRepo.floPathFwd = [imRepo.root, 'fullFlow/davisforward/'];
 imRepo.imgPath = [imRepo.root, 'JPEGImages/'];
-imRepo.fgHeatMap = [imRepo.root, 'OpticalFlowMinMax/'];
+imRepo.fgHeatMap = [imRepo.root, 'OpticalFlow/'];
 
 imRepo.scale = [480, 854];     % resize the image to make it tractable
 % This is the same scale we used when estimating optical flow
 imRepo.typ = 'jpg';
-alpha = 4;
 
-% minU = 0;
-% minV = 0;
-% maxU = 0;
-% maxV = 0;
 load minmax.mat;
 drp = dir(imRepo.imgPath);      % A list of all the seqeunces
 for k=3:length(drp) % check k should start with 3 or 4 (depends on OS)
@@ -63,10 +49,8 @@ for k=3:length(drp) % check k should start with 3 or 4 (depends on OS)
         saveNameX = sprintf('%s/flow_x_%s', saveDir, dr(i-1).name);
         saveNameY = sprintf('%s/flow_y_%s', saveDir, dr(i-1).name);
         imwrite(uint8(u), saveNameX);
-        imwrite(uint8(v), saveNameY);
-        
+        imwrite(uint8(v), saveNameY); 
     end
-    
 end
 
 
